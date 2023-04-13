272a475cc4cc59608c2554d71f26d3620e25e215
jason <890317jason@gmail.com>
2023-04-13 17:18:22 +0800

    tickets_1

--- ticket.py
+++ ticket.py
@@ -1,3 +1,12 @@
 from selenium import webdriver
+import time 
+
 driver = webdriver.Chrome('./chromedriver')
-driver.get('https://leetcode.com/')
+driver.get('https://ticketplus.com.tw/activity/e1d789a22aaaf3d52b88801b0ce9f860')
+driver.maximize_window()
+time.sleep(2)
+driver.find_element("xpath","//*[@id='appBar']/div/div/div/button").click()
+
+
+
+time.sleep(100)
